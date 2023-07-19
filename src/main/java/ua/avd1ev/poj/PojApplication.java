fa54dc38074ba9931899d7b7f99753660f987c83
avd1ev <avdeev10v@gmail.com>
2023-07-19 23:37:12 +0300

    init commit

---
+++ src/main/java/ua/avd1ev/poj/PojApplication.java
@@ -0,0 +1,13 @@
+package ua.avd1ev.poj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PojApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PojApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ua/avd1ev/poj/PojApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.avd1ev.poj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PojApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
